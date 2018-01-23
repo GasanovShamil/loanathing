a011b2550fbc74b0567a5000ee14e187a1f17f53
romainbor <bordier.romain@yahoo.fr>
2018-01-23 05:20:27 +0100

    user repo

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  * @ORM\Table(name="fos_user")
  */
 class User extends BaseUser {
---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * UserRepository
+ */
+class UserRepository extends EntityRepository
+{
+    
+}
